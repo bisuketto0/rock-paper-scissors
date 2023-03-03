1c50b8359388338ab5b87a0eea70f39f87a1dfac
Daniel <123590342+bisuketto0@users.noreply.github.com>
2023-03-03 10:45:00 -0300

    Create function to play round

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getPlayerChoice() {
   return prompt('Chose: Rock, Paper or Scissors').toLowerCase();
 }
-//Get players choice by asking them to choose between rock, paper or scissors.
+//Get player's choice by asking them to choose between rock, paper or scissors.
 
 function getComputerChoice() {
   const randomChoice = Math.floor(Math.random() * 3) + 1
@@ -13,3 +13,20 @@ function getComputerChoice() {
     return 'scissors';
   }
 }
+//Get computer's choice by randomly choosing between rock, paper and scissors.
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = getPlayerChoice();
+  computerSelection = getComputerChoice();
+
+  if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+    return 'PlayerWins';
+  
+  } else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'rock')) {
+    return 'PlayerLoses';
+  
+  } else {
+    return 'Draw';
+  }
+}
+//Compare player and computer choice and check who won
