deb2a5a97bea389a861a5e6e28223704cf87a076
Daniel <123590342+bisuketto0@users.noreply.github.com>
2023-03-03 10:05:28 -0300

    Create function for pc choice

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function getPlayersChoice() {
-  return prompt('Chose: Rock, Paper or Scissors').toLowerCase()
+function getPlayerChoice() {
+  return prompt('Chose: Rock, Paper or Scissors').toLowerCase();
 }
-
 //Get players choice by asking them to choose between rock, paper or scissors.
+
+function getComputerChoice() {
+  const randomChoice = Math.floor(Math.random() * 3) + 1
+  if (randomChoice === 1) {
+    return 'rock';
+  } else if (randomChoice === 2) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
