1eac26ad7f77d0b94866bdb733817e2fde76e27a
Daniel <123590342+bisuketto0@users.noreply.github.com>
2023-03-11 11:16:45 -0300

    Make score update every round

--- script.js
+++ script.js
@@ -17,24 +17,33 @@ function getComputerChoice() {
   }
 } //Get computer's choice by randomly choosing between rock, paper and scissors.
 
+let playerScore = 0;
+let computerScore = 0;  
 
 function playRound(playerSelection) {
-  
+
   computerSelection = getComputerChoice();
 
   if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+    playerScore += 1;
     showResults('Player Wins!');
   
   } else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'rock')) {
+    computerScore += 1;
     showResults('Player Loses!');
   
   } else {
     showResults("It's a Draw!");
   }
+  showScore(playerScore, computerScore);
 } //Compare player and computer choice and check who won
 
 function showResults(message) {
-  const results = document.querySelector('.results')
   const result = document.querySelector('.result')
   result.textContent = message
+}
+
+function showScore(playerScore, computerScore) {
+  const score = document.querySelector('.score')
+  score.textContent = `The score is ${playerScore} X ${computerScore}`
 }
